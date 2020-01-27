0885635504fbaea89daa50f5cc7ea2af6be10cf3
Dikhit <dikhitbhuyan@gmail.com>
2020-01-28 04:26:31 +0530

    update data and search data is added in employee data management

--- python_projects/employee_data_management/index.py
+++ python_projects/employee_data_management/index.py
@@ -39,6 +39,66 @@ class employee_data_management():
         except :
             print("close")
             return False
+    def update_data(self,email="example@emaple.com"):
+        if not email == "example@emaple.com":
+            all_users = self.read_data()
+            for items in all_users:
+                if items[1] == email:
+                    print("\n\nyour name is : ", items[0])
+                    print("your email is : ", items[1])
+                    print("your password is : ", items[2])
+                    ans = input("\nWhat you want to change (name/email/password) : " )
+                    
+                    if ans == "name":
+                        name = input("\nEnter your name : ")
+                        old_name = items[0]
+                        if len(name) > 0:
+                            items[0] = name
+                            with open(self.database,'r') as file:
+                                filedata = file.read()
+                            filedata = filedata.replace(old_name,items[0])
+                            with open(self.database,'w') as file:
+                                file.write(filedata)
+                            print("your name is successfully changed")
+                        else:
+                            return "Wrong Input"
+                             
+                    
+                    elif ans == "email":
+                        email = input("\nEnter your email : ")
+                        old_email = items[1]
+                        if len(email) > 0:
+                            items[1] = email
+                            with open(self.database,'r') as file:
+                                filedata = file.read()
+                            filedata = filedata.replace(old_email,items[1])
+                            with open(self.database,'w') as file:
+                                file.write(filedata)
+
+                            print("your email is successfully changed") 
+                        else:
+                            return "Wrong Input"
+
+                    elif ans == "password":
+                        password = input("\nEnter your password : ")
+                        old_password = items[2]
+                        if len(password) > 0:
+                            items[2] = password
+                            with open(self.database,'r') as file:
+                                filedata = file.read()
+                            filedata = filedata.replace(old_password,items[1])
+                            with open(self.database,'w') as file:
+                                file.write(filedata)
+                            print("your password is successfully changed")
+                        else:
+                            return "Wrong Input"
+            return all_users
+        return "Wrong Input"
+    
+    def search_user(self, serial_num = -1):
+        if not serial_num == -1:
+            all_users = self.read_data()
+            return all_users[serial_num - 1]
 
 
 obj = employee_data_management()
